44e007e20bd35fd4f41254811300ec789f0bc3c1
nomishkadilshan4 <nomishkadilshan4@gmail.com>
2024-05-27 21:54:52 +0530

    Edited the server.js file for solve the console error

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 import express from "express";
 import { Server } from "socket.io";
+import cors from "cors";
 
 const PORT = 8080;
 const app = express();
 const options = {
-  cors: true,
-  origin: ["http://localhost:8080"],
+  cors: {
+    origin: ["http://localhost:8080"],
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  }
+  
 };
 
 const server = app.listen(PORT, () => {
@@ -14,9 +22,18 @@ const server = app.listen(PORT, () => {
 
 const io = new Server(server, options);
 
+// Use cors middleware
+app.use(cors(options));
+
 app.use(express.static("./dist"));
 
+app.options("*", (req, res) => {
+  res.set("Access-Control-Allow-Private-Network", "true");
+  res.sendStatus(204);
+});
+
 app.get("/", (req, res) => {
+  res.set("Access-Control-Allow-Private-Network", "true");
   res.sendFile("index.html");
 });
 

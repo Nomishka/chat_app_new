6a99426f00da4f7da010e9b047e404bf5bd8d7d8
nomishka <nomishkadilshan4@gmail.com>
2024-05-28 17:45:46 +0530

    Edited package.json and server.js

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 import express from "express";
 import { Server } from "socket.io";
-import cors from "cors";
 
 const PORT = 8080;
 const app = express();
 const options = {
-  cors: {
-    origin: ["http://localhost:8080"],
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type"],
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true,
-  }
-  
+  cors: true,
+  origin: ["http://localhost:8080"]
 };
 
 const server = app.listen(PORT, () => {
@@ -22,18 +14,11 @@ const server = app.listen(PORT, () => {
 
 const io = new Server(server, options);
 
-// Use cors middleware
-app.use(cors(options));
 
-app.use(express.static("./dist"));
 
-app.options("*", (req, res) => {
-  res.set("Access-Control-Allow-Private-Network", "true");
-  res.sendStatus(204);
-});
+app.use(express.static("./docs"));
 
 app.get("/", (req, res) => {
-  res.set("Access-Control-Allow-Private-Network", "true");
   res.sendFile("index.html");
 });
 

d85f259c28646d00f378b3bf280478ecb2d26759
nomishka <nomishkadilshan4@gmail.com>
2024-05-30 14:56:06 +0530

    Changed clientside socket

--- server.js
+++ server.js
@@ -3,28 +3,16 @@ import { Server } from "socket.io";
 
 const PORT = 8080;
 const app = express();
+const options = {
+  cors: true,
+  origin: ["https://chat-app-new-m2ui.onrender.com", "http://localhost8080"],
+};
 
 const server = app.listen(PORT, () => {
   console.log("server is started on port", PORT);
 });
 
-const cors = require('cors');
-
 const io = new Server(server, options);
-const options = {
-  origin: [
-    "https://chat-app-new-m2ui.onrender.com",
-    "https://nomishka.github.io/chat_app_new",
-  ],
-  methods: ["GET", "POST"],
-  credentials: true,
-};
-
-app.use((req, res, next) => {
-  res.setHeader("Permissions-Policy", "interest-cohort=()");
-});
-
-app.use(cors(options));
 
 app.use(express.static("./docs"));
 

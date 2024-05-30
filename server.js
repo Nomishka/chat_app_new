6714d511f96eae17ac07eb5240b1ea8c4f19a16b
nomishka <nomishkadilshan4@gmail.com>
2024-05-30 14:02:58 +0530

    Added multiple origins to allow from server

--- server.js
+++ server.js
@@ -4,16 +4,36 @@ import { Server } from "socket.io";
 const PORT = 8080;
 const app = express();
 
+const allowedOrigins = [
+  "https://chat-app-new-m2ui.onrender.com",
+  "https://github.com/Nomishka/chat_app_new"
+];
+
 const server = app.listen(PORT, () => {
   console.log("server is started on port", PORT);
 });
 
 const io = new Server(server, {
   cors: {
-    origin: "https://chat-app-new-m2ui.onrender.com",
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST"],
     credentials: true
   }
+})
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.setHeader("Permissions-Policy", "interest-cohort=()");
+  next();
 })
 
 app.use(express.static("./docs"));

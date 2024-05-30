faadbab1583f1c2865ce0d1652fa8b31fcb142f5
nomishka <nomishkadilshan4@gmail.com>
2024-05-30 14:41:08 +0530

    Changed cors configuration

--- server.js
+++ server.js
@@ -4,37 +4,27 @@ import { Server } from "socket.io";
 const PORT = 8080;
 const app = express();
 
-const allowedOrigins = [
-  "https://chat-app-new-m2ui.onrender.com",
-  "https://nomishka.github.io/chat_app_new"
-];
-
 const server = app.listen(PORT, () => {
   console.log("server is started on port", PORT);
 });
 
-const io = new Server(server, {
-  cors: {
-    origin: (origin, callback) => {
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST"],
-    credentials: true
-  }
-})
+const cors = require('cors');
+
+const io = new Server(server, options);
+const options = {
+  origin: [
+    "https://chat-app-new-m2ui.onrender.com",
+    "https://nomishka.github.io/chat_app_new",
+  ],
+  methods: ["GET", "POST"],
+  credentials: true,
+};
 
 app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
   res.setHeader("Permissions-Policy", "interest-cohort=()");
-  next();
-})
+});
+
+app.use(cors(options));
 
 app.use(express.static("./docs"));
 
